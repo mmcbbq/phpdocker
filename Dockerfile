6df52d3033c2167f45047b1da0318ded9783b30e
mmcbbq <m.martinez@bbq.de>
2024-04-09 11:41:59 +0200

    add comments

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
+# Als Basis Image php:7.2-apache eine PHP image mit eingebauten apache
 FROM php:7.2-apache
+# installieren von mysqli
 RUN docker-php-ext-install mysqli
+# Kopieren der Files . (aktuelles Verzeichnis) vom Host in den Container /var/www/html (apache htdoc)
 COPY . /var/www/html
--- phpinfo.php
+++ phpinfo.php
@@ -1,15 +1,15 @@
 <?php
 //phpinfo();
-$servername= "mariadb";
-$username= "manuel";
-$password= "123456";
-$db = 'hallo_db';
+$servername = "mariadb"; # nicht localhost, sondern aus der compose.yaml
+$username = "manuel"; # ebenso aus der compose.yaml
+$password = "123456";  # ebenso aus der compose.yaml
+$db = 'hallo_db'; # ebenso aus der compose.yaml
 
 $conn = new mysqli($servername, $username, $password, $db);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else{
+} else {
     echo "alles ok";
 }
 
